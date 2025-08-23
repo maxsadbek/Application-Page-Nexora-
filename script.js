0da01ef47e4a63a0721907df023594341a37cd5e
maxsad_bek <masadanb@gmail.com>
2025-08-23 14:35:40 +0500

    add

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let btn = document.getElementById("menuBtn");
 let menu = document.getElementById("sideMenu");
-let CloseBtn = document.querySelector("#close")
+let CloseBtn = document.querySelector("#close");
 let dropdown1Btn = document.querySelector("#dropdown1");
 let dropdown2Box = document.querySelector("#dropdown2");
 
+// dropdown
 dropdown1Btn.addEventListener("click", () => {
   dropdown2Box.classList.toggle("open");
 });
 
+// close bosilganda yopiladi
 CloseBtn.addEventListener("click", () => {
-    menu.classList.remove("open");
+  menu.classList.remove("open");
+});
 
 
-})
 btn.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
