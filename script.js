86af82d7e7b0341bafab96ad8e775b67dba52f9e
maxsad_bek <masadanb@gmail.com>
2025-08-23 11:29:34 +0500

    add

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let btn = document.getElementById("menuBtn");
 let menu = document.getElementById("sideMenu");
 let CloseBtn = document.querySelector("#close")
 
+
+
+
 CloseBtn.addEventListener("click", () => {
     menu.classList.remove("open");
 })
