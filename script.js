0388f34dce3f1593095be170847fefd0e88d4099
maxsad_bek <masadanb@gmail.com>
2025-08-23 11:54:16 +0500

    add

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let btn = document.getElementById("menuBtn");
 let menu = document.getElementById("sideMenu");
 let CloseBtn = document.querySelector("#close")
+let dropdown1Btn = document.querySelector("#dropdown1");
+let dropdown2Box = document.querySelector("#dropdown2");
 
-
-
+dropdown1Btn.addEventListener("click", () => {
+  dropdown2Box.classList.toggle("open");
+});
 
 CloseBtn.addEventListener("click", () => {
     menu.classList.remove("open");
+
+
 })
 btn.addEventListener("click", () => {
   menu.classList.toggle("open");
